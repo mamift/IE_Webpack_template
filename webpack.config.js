74eabe05f1cb18df4938c161653089ce66ff69a1
Muhammad Miftah <mamift@gmail.com>
2019-09-25 13:27:11 +1000

    Added file-loader for svg files (url loader cant seem to handle them).

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,14 @@ module.exports = {
       {
         test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
         loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.(svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'dist/'
+        }
       }
     ]
   },
